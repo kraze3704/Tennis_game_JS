6998929db970707dab468eec8eb579428a8be965
changHyunKim <kraze3704@gmail.com>
2019-09-10 21:47:37 +0300

    additional shapes, FPS added to code

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 
 let canvas, canvasContext;
+let ballX = 75, ballY = 75;
+const FPS = 30;
 
 window.onload = () => {
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
 
+    setInterval(function() {
+        moveAll();
+        drawAll();
+    }, 1000/FPS);
+}
+
+moveAll = () => {
+    ballX += 2;
+}
+
+drawAll = () => {
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+    canvasContext.fillStyle = 'white'; // sets the color
+    canvasContext.beginPath(); // begins a new seperate shape
+    canvasContext.arc(ballX, ballY, 10, 0, Math.PI*2, true); // x, y coordinates of center, radius, start & end angles, counterclockwise(true)
+    canvasContext.fill(); // fills the shape drawn since beginPath()
 }
