ca54158d61ab021ffe82ba88ef095a5fe378a56e
changHyunKim <kraze3704@gmail.com>
2019-09-10 22:23:24 +0300

    left paddle moves according to mouse

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 let canvas, canvasContext;
-let ballX = 75, ballY = 75, ballRadius = 10;
+
+let ballX = 75, ballY = 75;
+const ballRadius = 10;
+let PADDLE_Y = 250;
+const PADDLE_HEIGHT = 100, PADDLE_WIDTH = 10;
+
 const FPS = 30;
 let ballSpeedX = 2, ballSpeedY = 2;
 
@@ -8,10 +13,28 @@ window.onload = () => {
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
 
+    canvas.addEventListener('mousemove', function(evt) {
+        let mousePos = calculateMousePos(evt);
+        PADDLE_Y = mousePos.y - (PADDLE_HEIGHT/2);
+    });
+
     setInterval(function() {
         moveAll();
         drawAll();
     }, 1000/FPS);
+}
+
+calculateMousePos = (evt) => {
+    let rect = canvas.getBoundingClientRect(), root = document.documentElement;
+
+    // account for margins, canvas position on page, scroll amount etc.
+    let mouseX = evt.clientX - rect.left - root.scrollLeft;
+    let mouseY = evt.clientY - rect.top - root.scrollTop;
+
+    return {
+        x: mouseX,
+        y: mouseY
+    };
 }
 
 moveAll = () => {
@@ -25,12 +48,25 @@ moveAll = () => {
     ballY += ballSpeedY;
 }
 
-drawAll = () => {
-    canvasContext.fillStyle = 'black';
-    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+colorRect = (topLeftX, topLeftY, boxWidth, boxHeight, fillColor) => {
+    canvasContext.fillStyle = fillColor;
+    canvasContext.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
+}
 
-    canvasContext.fillStyle = 'white'; // sets the color
-    canvasContext.beginPath(); // begins a new seperate shape
-    canvasContext.arc(ballX, ballY, ballRadius, 0, Math.PI*2, true); // x, y coordinates of center, radius, start & end angles, counterclockwise(true)
-    canvasContext.fill(); // fills the shape drawn since beginPath()
+colorCircle = (centerX, centerY, radius, fillColor) => {
+    canvasContext.fillStyle = fillColor;
+    canvasContext.beginPath();
+    canvasContext.arc(centerX, centerY, radius, 0, Math.PI *2, true);
+    canvasContext.fill();
+}
+
+drawAll = () => {
+    // clear game view by filling it black
+    colorRect(0, 0, canvas.width, canvas.height, '#000000');
+
+    // draw white rectangle to use as left player's paddle
+    colorRect(0, PADDLE_Y, PADDLE_WIDTH, PADDLE_HEIGHT, '#FFFFFF');
+
+    // draw the ball
+    colorCircle(ballX, ballY, 10, '#FFFFFF');
 }
